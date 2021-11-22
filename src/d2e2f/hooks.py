ecc7a9a63ae7f78da49ae49d7500f69a4c256878
Martin Alexandersson <martin.alexandersson@sspa.se>
2021-11-22 09:24:03 +0100

    safe commit.

--- src/d2e2f/hooks.py
+++ src/d2e2f/hooks.py
@@ -38,7 +38,10 @@ from kedro.versioning import Journal
 class ProjectHooks:
     @hook_impl
     def register_config_loader(
-        self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any],
+        self,
+        conf_paths: Iterable[str],
+        env: str,
+        extra_params: Dict[str, Any],
     ) -> ConfigLoader:
         return ConfigLoader(conf_paths)
 
@@ -54,3 +57,44 @@ class ProjectHooks:
         return DataCatalog.from_config(
             catalog, credentials, load_versions, save_version, journal
         )
+
+
+from typing import Any, Dict
+
+import mlflow
+import mlflow.sklearn
+from kedro.framework.hooks import hook_impl
+from kedro.pipeline.node import Node
+
+
+class ModelTrackingHooks:
+    """Namespace for grouping all model-tracking hooks with MLflow together."""
+
+    @hook_impl
+    def before_pipeline_run(self, run_params: Dict[str, Any]) -> None:
+        """Hook implementation to start an MLflow run
+        with the same run_id as the Kedro pipeline run.
+        """
+        mlflow.start_run(run_name=run_params["run_id"])
+        mlflow.log_params(run_params)
+
+    @hook_impl
+    def after_node_run(
+        self, node: Node, outputs: Dict[str, Any], inputs: Dict[str, Any]
+    ) -> None:
+        """Hook implementation to add model tracking after some node runs.
+        In this example, we will:
+        * Log the parameters after the data splitting node runs.
+        * Log the model after the model training node runs.
+        * Log the model's metrics after the model evaluating node runs.
+        """
+        if node._func_name == "fit_linear_regression":
+            model = outputs["trip_statistics_joined_thrusters_linear_regression_model"]
+            mlflow.sklearn.log_model(model, "model")
+
+    @hook_impl
+    def after_pipeline_run(self) -> None:
+        """Hook implementation to end the MLflow run
+        after the Kedro pipeline finishes.
+        """
+        mlflow.end_run()
